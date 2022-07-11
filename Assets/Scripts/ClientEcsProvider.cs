0cae17bbc851cef28a876990bb7a175c0fc9cfe1
Mikhail Kryloff <mikhail.krylov@playgendary.com>
2022-07-11 22:05:25 +0500

    Refactoring

---
+++ Assets/Scripts/ClientEcsProvider.cs
@@ -0,0 +1,40 @@
+﻿using RiptideNetworking;
+using UnityEngine;
+
+namespace GameLogic
+{
+    public class ClientEcsProvider : BaseEcsProvider
+    {
+        public Client Client
+        {
+            set
+            {
+                if (inited)
+                {
+                    return;
+                }
+
+                inited = true;
+                systems.Inject(value);
+                systems.Init();
+            } 
+        }
+
+        private bool inited = false;
+        
+        protected override void AddOneFrames()
+        {
+            base.AddOneFrames();
+
+            systems.OneFrame<CharacterMovementUpdate>();
+        }
+
+        protected override void AddSystems()
+        {
+            base.AddSystems();
+
+            systems.Add(new UpdateSendSystem());
+            systems.Add(new NetworkSendSystem());
+        }
+    }
+}
